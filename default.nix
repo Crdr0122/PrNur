a7006c64b436594c555906beff6a56fc0e383dae
Crdr0122 <161446951+Crdr0122@users.noreply.github.com>
2024-07-15 16:35:40 +0900

    Update default.nix

--- default.nix
+++ default.nix
@@ -15,6 +15,7 @@
   overlays = import ./overlays; # nixpkgs overlays
 
   example-package = pkgs.callPackage ./pkgs/example-package { };
+  quickq = pkgs.callPackage ./pkgs/quickq { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }

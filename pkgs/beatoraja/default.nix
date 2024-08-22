0ed9832b83caee66e1a81daec545d0079a00edd5
Crdr0122 <161446951+Crdr0122@users.noreply.github.com>
2024-08-22 20:03:13 +0900

    Update default.nix

--- pkgs/beatoraja/default.nix
+++ pkgs/beatoraja/default.nix
@@ -1,78 +1,74 @@
-{ stdenv
-, fetchurl
-, unzip
-, oraclejre8
+{ lib
+, stdenv
+, fetchzip
+, jdk
+, writeShellScript
+, openal
+, xrandr
 , makeWrapper
 , libjportaudio
-, xrandr
 }:
 
 let
   pname = "beatoraja-modernchic";
   version = "0.8.7";
   fullName = "beatoraja${version}-modernchic";
-  jre = oraclejre8.overrideAttrs {
-    src = fetchTarball {
-      url = "https://static.sora.zip/nix/jdk-8u281-linux-x64.tar.gz";
-      sha256 = "0f9fb37p75cf7qfm67yc8ariqksnw8641kh2zcwvlrr4r8lgj70v";
-    };
-  };
+  startupScript = writeShellScript "beatoraja.sh" ''
+    export _JAVA_OPTIONS='-Dsun.java2d.opengl=true -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+    dataDir="''${XDG_DATA_HOME:-$HOME/.local/share}/beatoraja"
+    if [ ! -d "$dataDir" ]; then
+      mkdir -p "$dataDir"
+      cp -r $out/opt/beatoraja/* "$dataDir"
+      find "$dataDir" -type f -exec chmod 644 {} \;
+      find "$dataDir" -type d -exec chmod 755 {} \;
+    fi
+    cd "''${XDG_DATA_HOME:-$HOME/.local/share}/beatoraja"
+    exec ${jdk.override { enableJavaFX = true; }}/bin/java -Xms1g -Xmx4g -jar $out/opt/beatoraja/beatoraja.jar $@
+  '';
+
 in
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   inherit pname version;
   name = "${pname}-${version}";
-    src = fetchurl {
+  src = fetchzip {
     url = "https://mocha-repository.info/download/${fullName}.zip";
-    sha256 = "sha256-rkM8z9Oyqy3dS9zWgTQyjeEQg7Nax1V5hohL/neCNr8="; # 可以先不填，后面根据报错给的实际值填上
-    };
-    nativeBuildInputs = [ unzip makeWrapper libjportaudio];
-  unpackPhase = "unzip $src"; # $src对应获取到的文件
-    preInstall = ''
-    rm ${fullName}/beatoraja-config.* # 删除原有的启动脚本
-    echo "#!/bin/sh" > ${fullName}/beatoraja.sh # 重新创建启动脚本
+    hash = "sha256-pQo/jWIMMT6btwsaO6wJbAPRCun+44GUPk0NMVpQcyc=";
+  };
 
-    echo 'if [ ! -d "''${XDG_DATA_HOME:-$HOME/.local/share}/beatoraja" ]; then' >> ${fullName}/beatoraja.sh # 如果不存在beatoraja的数据目录
+  nativeBuildInputs = [ makeWrapper ];
 
-    echo 'mkdir -p "''${XDG_DATA_HOME:-$HOME/.local/share}/beatoraja"'          >> ${fullName}/beatoraja.sh # 创建数据目录
-    echo 'cd "''${XDG_DATA_HOME:-$HOME/.local/share}/beatoraja"'                >> ${fullName}/beatoraja.sh # 进入数据目录
-
-    # 复制相关文件
-    echo "cp -r $out/share/beatoraja/bgm ./"          >> ${fullName}/beatoraja.sh
-    echo "cp -r $out/share/beatoraja/defaultsound ./" >> ${fullName}/beatoraja.sh
-    echo "cp -r $out/share/beatoraja/folder ./"       >> ${fullName}/beatoraja.sh
-    echo "cp -r $out/share/beatoraja/ir ./"           >> ${fullName}/beatoraja.sh
-    echo "cp -r $out/share/beatoraja/skin ./"         >> ${fullName}/beatoraja.sh
-    echo "cp -r $out/share/beatoraja/sound ./"        >> ${fullName}/beatoraja.sh
-    echo "cp -r $out/share/beatoraja/table ./"        >> ${fullName}/beatoraja.sh
-
-    # 修改权限
-    echo "find . -type d -exec chmod 755 {} \;"       >> ${fullName}/beatoraja.sh
-    echo "find . -type f -exec chmod 644 {} \;"       >> ${fullName}/beatoraja.sh
-
-    echo "fi" >> ${fullName}/beatoraja.sh
-
-    echo 'cd "''${XDG_DATA_HOME:-$HOME/.local/share}/beatoraja"' >> ${fullName}/beatoraja.sh # 确保每次执行的工作目录
-
-    # 这里通过内联oraclejre8能够直接获取到其在/nix/store中的路径
-    echo "exec ${jre}/bin/java -Xms1g -Xmx4g -jar '$out/share/beatoraja/beatoraja.jar'" >> ${fullName}/beatoraja.sh
-    chmod +x ${fullName}/beatoraja.sh # 赋予执行权限
+  preInstall = ''
+    rm beatoraja-config.bat
+    rm beatoraja-config.command
+    rm jportaudio_x64.dll
+    rm portaudio_x64.dll
   '';
 
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/share/beatoraja
+    mkdir -p $out/opt/beatoraja
     mkdir -p $out/bin
-    mv ${fullName}/beatoraja.sh $out/bin/beatoraja
-    mv ${fullName}/* $out/share/beatoraja
+    ln -s ${startupScript} $out/bin/beatoraja
+    mv * $out/opt/beatoraja/
 
-    # prefix: 在原有的环境变量前添加
     wrapProgram $out/bin/beatoraja \
+      --prefix PATH : "${xrandr}/bin" \
+      --set out $out \
+      --prefix LD_LIBRARY_PATH : "${openal}/lib" \
       --prefix LD_LIBRARY_PATH : "${libjportaudio}/lib" \
-      --prefix LD_PRELOAD : "${libjportaudio}/lib/libjportaudio.so" \
-       --prefix PATH : "${xrandr}/bin" \
-      --prefix _JAVA_OPTIONS : "-Dsun.java2d.opengl=true -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel"
+      --prefix LD_PRELOAD : "${openal}/lib/libopenal.so" \
+      --prefix LD_PRELOAD : "${libjportaudio}/lib/libjportaudio.so"
 
     runHook postInstall
   '';
+
+  meta = with lib; {
+    description = "Cross-platform rhythm game based on Java and libGDX.";
+    homepage = "https://github.com/exch-bms2/beatoraja";
+    license = [ licenses.gpl3Only "unknown" ];
+    platforms = [ "x86_64-linux" ];
+    mainProgram = "beatoraja";
+    sourceProvenance = with sourceTypes; [ binaryBytecode ];
+  };
 }

07d8fabf2e8c9a823216ebad8ce2f3f7aace0a29
Crdr0122 <161446951+Crdr0122@users.noreply.github.com>
2024-08-25 12:43:06 +0900

    Update default.nix

--- pkgs/libjportaudio/default.nix
+++ pkgs/libjportaudio/default.nix
@@ -1,37 +1,29 @@
 { stdenv
 , fetchFromGitHub
-, oraclejdk8 # 废话
-, cmake      # 使用cmake进行构建
-, portaudio  # 依赖libportaudio.so
+, jdk
+, cmake
+, portaudio
 }:
 
 let
   version = "0.1.0";
   pname = "libjportaudio";
-  jdk = oraclejdk8.overrideAttrs {
-    src = fetchTarball {
-      url = "https://static.sora.zip/nix/jdk-8u281-linux-x64.tar.gz";
-      sha256 = "0f9fb37p75cf7qfm67yc8ariqksnw8641kh2zcwvlrr4r8lgj70v";
-    };
-  };
 in
 stdenv.mkDerivation rec {
   inherit version pname;
   name = "${pname}-${version}";
-  src = fetchFromGitHub ({
+  src = fetchFromGitHub {
     owner = "philburk";
     repo = "portaudio-java";
     rev = "2ec5cc47d6f8abe85ddb09c34e69342bfe72c60b";
     sha256 = "t+Pqtgstd1uJjvD4GKomZHMeSECNLeQJOrz97o+lV2Q=";
-  });
+  };
 
   nativeBuildInputs = [ cmake portaudio ];
 
   preConfigure = ''
     export JAVA_HOME=${jdk}
   '';
-
-  # configurePhase会自动执行cmake
 
   buildPhase = ''
     make jportaudio_0_1_0

0b9f9d2463cc6453df6fb75162a37f362cc11cfb
Yuken <yukenharimoto@outlook.com>
2024-10-23 23:44:39 +0800

    wechatFail

--- default.nix
+++ default.nix
@@ -20,9 +20,6 @@ rec {
   beatoraja = pkgs.callPackage ./pkgs/beatoraja { libjportaudio = libjportaudio; };
   libjportaudio = pkgs.callPackage ./pkgs/libjportaudio { };
   vte-sixel = pkgs.callPackage ./pkgs/vte-sixel { };
-  wechat-universal = pkgs.callPackage ./pkgs/wechat-universal { };
-  wechat-uos = pkgs.callPackage ./pkgs/wechat-uos { };
-  wechat-wine = pkgs.callPackage ./pkgs/wechat-wine { };
   # xfce4-terminal = pkgs.callPackage ./pkgs/xfce4-terminal{ };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
